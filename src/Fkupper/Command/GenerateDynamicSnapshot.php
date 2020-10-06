c0806d2df1578a2f106e9875a745756dfec5cf42
Fernando Kupper Cardoso <fernando@kupper.com.br>
2020-10-06 22:43:36 +0200

    add command to generate dynamic snapshots

---
+++ src/Fkupper/Command/GenerateDynamicSnapshot.php
@@ -0,0 +1,81 @@
+<?php
+namespace Fkupper\Command;
+
+use Codeception\Command\Shared\Config;
+use Codeception\Command\Shared\FileSystem;
+use Codeception\Configuration;
+use Codeception\CustomCommandInterface;
+use Fkupper\Lib\Generator\DynamicSnapshot as DynamicSnapshotGenerator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generates DynamicSnapshot.
+ * DynamicSnapshot can be used to test dynamical data.
+ * If suite name is provided, an actor class will be included into placeholder
+ *
+ * * `codecept g:dynamicsnapshot UserEmails`
+ * * `codecept g:dynamicsnapshot Products`
+ * * `codecept g:dynamicsnapshot acceptance UserEmails`
+ */
+class GenerateDynamicSnapshot extends Command implements CustomCommandInterface
+{
+    use FileSystem;
+    use Config;
+
+    public static function getCommandName()
+    {
+        return 'generate:dynamicsnapshot';
+    }
+
+    protected function configure()
+    {
+        $this->setDefinition([
+            new InputArgument('suite', InputArgument::REQUIRED, 'Suite name or snapshot name)'),
+            new InputArgument('dynamicsnapshot', InputArgument::OPTIONAL, 'Name of snapshot'),
+        ]);
+        parent::configure();
+    }
+
+    public function getDescription()
+    {
+        return 'Generates empty DynamicSnapshot class';
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $suite = $input->getArgument('suite');
+        $class = $input->getArgument('dynamicsnapshot');
+
+        if (!$class) {
+            $class = $suite;
+            $suite = null;
+        }
+
+        $conf = $suite
+            ? $this->getSuiteConfig($suite)
+            : $this->getGlobalConfig();
+
+        if ($suite) {
+            $suite = DIRECTORY_SEPARATOR . ucfirst($suite);
+        }
+
+        $path = $this->createDirectoryFor(Configuration::supportDir() . 'Snapshot' . $suite, $class);
+
+        $filename = $path . $this->getShortClassName($class) . '.php';
+
+        $output->writeln($filename);
+
+        $gen = new DynamicSnapshotGenerator($conf, ucfirst($suite) . '\\' . $class);
+        $res = $this->createFile($filename, $gen->produce());
+
+        if (!$res) {
+            $output->writeln("<error>DynamicSnapshot $filename already exists</error>");
+            return 1;
+        }
+        $output->writeln("<info>DynamicSnapshot was created in $filename</info>");
+        return 0;
+    }
+}
---
+++ src/Fkupper/Lib/Generator/DynamicSnapshot.php
@@ -0,0 +1,42 @@
+<?php
+namespace Fkupper\Lib\Generator;
+
+use Codeception\Lib\Generator\Snapshot;
+use Codeception\Util\Shared\Namespaces;
+
+class DynamicSnapshot extends Snapshot
+{
+    use Namespaces;
+
+    protected $template = <<<EOF
+<?php
+namespace {{namespace}};
+
+class {{name}} extends \\Codeception\\DynamicSnapshot
+{
+
+{{actions}}
+
+    protected function fetchDynamicData()
+    {
+        // TODO: return a value which will be used for snapshot
+        // OPTIONALLY: set the substitutions
+        \$this->setSubstitutions([
+            'key' => 'value',
+        ]);
+    }
+}
+EOF;
+
+    protected $actionsTemplate = <<<EOF
+    /**
+     * @var \\{{actorClass}};
+     */
+    protected \${{actor}};
+
+    public function __construct(\\{{actorClass}} \$I)
+    {
+        \$this->{{actor}} = \$I;
+    }
+EOF;
+}
