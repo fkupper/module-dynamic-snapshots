2a3da1f5cba992086cae031633364ee110c2241c
countless-integers <oxygen-nitrogen@outlook.com>
2023-02-20 13:23:18 +0100

    fix(GenerateDynamicSnapshot): update trait names
    chore(GenerateDynamicSnapshot): cast options to string to prevent any weird behaviour
    
    fix(GenerateDynamicSnapshot): add method return types

--- src/Fkupper/Command/GenerateDynamicSnapshot.php
+++ src/Fkupper/Command/GenerateDynamicSnapshot.php
@@ -2,8 +2,8 @@
 
 namespace Fkupper\Command;
 
-use Codeception\Command\Shared\Config;
-use Codeception\Command\Shared\FileSystem;
+use Codeception\Command\Shared\ConfigTrait;
+use Codeception\Command\Shared\FileSystemTrait;
 use Codeception\Configuration;
 use Codeception\CustomCommandInterface;
 use Fkupper\Lib\Generator\DynamicSnapshot as DynamicSnapshotGenerator;
@@ -23,10 +23,10 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class GenerateDynamicSnapshot extends Command implements CustomCommandInterface
 {
-    use FileSystem;
-    use Config;
+    use FileSystemTrait;
+    use ConfigTrait;
 
-    public static function getCommandName()
+    public static function getCommandName(): string
     {
         return 'generate:dynamicsnapshot';
     }
@@ -40,15 +40,15 @@ class GenerateDynamicSnapshot extends Command implements CustomCommandInterface
         parent::configure();
     }
 
-    public function getDescription()
+    public function getDescription(): string
     {
         return 'Generates empty DynamicSnapshot class';
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $suite = $input->getArgument('suite');
-        $class = $input->getArgument('dynamicsnapshot');
+        $suite = (string)$input->getArgument('suite');
+        $class = (string)$input->getArgument('dynamicsnapshot');
 
         if (!$class) {
             $class = $suite;
@@ -61,6 +61,8 @@ class GenerateDynamicSnapshot extends Command implements CustomCommandInterface
 
         if ($suite) {
             $suite = DIRECTORY_SEPARATOR . ucfirst($suite);
+        } else {
+            $suite = '';
         }
 
         $path = $this->createDirectoryFor(Configuration::supportDir() . 'Snapshot' . $suite, $class);
